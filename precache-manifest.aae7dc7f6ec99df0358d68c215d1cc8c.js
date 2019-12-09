dc3fbd2dadd709314d9f162e24c5711f9544eb51
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2019-12-09 07:36:18 +0000

    Deploying to gh-pages from master cc59aa7775b74a1e8b21ec95c121e72f1c67382d

---
+++ precache-manifest.aae7dc7f6ec99df0358d68c215d1cc8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0088b9d155398de602d478b97d20c760",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "d77ec583c825975fffe1",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "19d9744383342afd88f5",
+    "url": "/github-actions-demo/static/js/2.063525e2.chunk.js"
+  },
+  {
+    "revision": "d77ec583c825975fffe1",
+    "url": "/github-actions-demo/static/js/main.f908ab29.chunk.js"
+  },
+  {
+    "revision": "cf2970a10973fa74b251",
+    "url": "/github-actions-demo/static/js/runtime~main.d2d38d17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
